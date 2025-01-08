3b4e1da104c804297b045de449c6f89fdfd30cac
lonkarpratiksha <lonkarpratiksha5@gmail.com>
2025-01-08 20:28:46 +0530

    category api created

---
+++ src/main/java/com/foodora/service/CategoryService.java
@@ -0,0 +1,14 @@
+package com.foodora.service;
+
+import java.util.List;
+
+import com.foodora.modal.Category;
+
+public interface CategoryService {
+    
+    public Category createCategory(String name,Long userId)throws Exception;
+
+    public List<Category> findCategoryByRestaurantId(Long id)throws Exception;
+
+    public Category findCategoryById(Long id)throws Exception;
+}
